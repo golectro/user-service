7cc610f6d4f8ceb7d67f4150183e4e6805daf6ef
Hackim18 <khakim668@gmail.com>
2025-08-04 16:34:44 +0700

    feat: add new seeders and migrations

--- internal/command/command.go
+++ internal/command/command.go
@@ -1,6 +1,7 @@
 package command
 
 import (
+	"golectro-user/internal/migrations"
 	"os"
 	"strings"
 
@@ -27,6 +28,10 @@ func (ce *CommandExecutor) Execute(logger *logrus.Logger) bool {
 		if strings.HasPrefix(arg, "--") {
 			hasFlag = true
 			switch arg {
+			case "--migrate":
+				ce.handleMigrate(logger)
+			case "--seed":
+				ce.handleSeed(logger)
 			case "--run":
 				run = true
 			}
@@ -42,4 +47,18 @@ func (ce *CommandExecutor) Execute(logger *logrus.Logger) bool {
 
 func contains(slice []string, target string) bool {
 	return slices.Contains(slice, target)
+}
+
+func (ce *CommandExecutor) handleMigrate(logger *logrus.Logger) {
+	if err := migrations.Migrate(ce.DB); err != nil {
+		logger.Fatalf("❌ Migration failed: %v", err)
+	}
+	logger.Println("✅ Migration completed")
+}
+
+func (ce *CommandExecutor) handleSeed(logger *logrus.Logger) {
+	if err := migrations.Seeder(ce.DB, logger); err != nil {
+		logger.Fatalf("❌ Seeder failed: %v", err)
+	}
+	logger.Println("✅ Seeder completed")
 }
