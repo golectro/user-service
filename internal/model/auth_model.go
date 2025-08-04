eeacddbfc2d5a8c847f4892763691a5be45c2a22
Hackim18 <khakim668@gmail.com>
2025-08-04 15:01:30 +0700

    feat: add new authentication model

---
+++ internal/model/auth_model.go
@@ -0,0 +1,10 @@
+package model
+
+import "github.com/google/uuid"
+
+type Auth struct {
+	ID       uuid.UUID `json:"id"`
+	Username string    `json:"username"`
+	Email    string    `json:"email"`
+	Roles    []string  `json:"roles"`
+}
