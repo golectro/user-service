6a0a033f3eb37948a7e2924585dfb85820f89cba
Hackim18 <khakim668@gmail.com>
2025-08-04 13:08:47 +0700

    feat: add logging middleware

---
+++ internal/delivery/http/middleware/logging_middleware.go
@@ -0,0 +1,72 @@
+package middleware
+
+import (
+	"context"
+	"golectro-user/internal/usecase"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"github.com/sirupsen/logrus"
+)
+
+func LoggingMiddleware(logger *logrus.Logger, logUC *usecase.LogUsecase) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		start := time.Now()
+		path := c.Request.URL.Path
+		query := c.Request.URL.RawQuery
+
+		c.Next()
+
+		end := time.Now()
+		latency := end.Sub(start)
+		reqID, _ := c.Get("requestId")
+
+		fields := logrus.Fields{
+			"status":    c.Writer.Status(),
+			"method":    c.Request.Method,
+			"path":      path,
+			"query":     query,
+			"ip":        c.ClientIP(),
+			"userAgent": c.Request.UserAgent(),
+			"latency":   latency,
+			"requestId": reqID,
+		}
+
+		if gin.Mode() == gin.DebugMode {
+			fields["headers"] = c.Request.Header
+		}
+
+		// auth := GetUser(c)
+		// var userID string
+		// if auth != nil {
+		// 	userID = auth.ID.String()
+		// }
+		userID := "123e4567-e89b-12d3-a456-426614174000" // Hardcoded UUID
+
+		if userID != "" {
+			if reqIDStr, ok := reqID.(string); ok {
+				go func(ctx context.Context, userID, path, reqID string, status int, errStr string) {
+					level := "INFO"
+					if status >= 500 {
+						level = "ERROR"
+					} else if status >= 400 {
+						level = "WARN"
+					}
+					if err := logUC.LogActivity(context.Background(), level, reqID, "Request processed", userID, path, status, errStr); err != nil {
+						logger.WithError(err).Warn("Failed to log activity to MongoDB")
+					}
+				}(c.Request.Context(), userID, path, reqIDStr, c.Writer.Status(), c.Errors.ByType(gin.ErrorTypePrivate).String())
+			}
+		}
+
+		entry := logger.WithFields(fields)
+		switch {
+		case c.Writer.Status() >= 500:
+			entry.Error("Request processed")
+		case c.Writer.Status() >= 400:
+			entry.Warn("Request processed")
+		default:
+			entry.Info("Request processed")
+		}
+	}
+}
