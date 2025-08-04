a1f2976fe9ee8674965b6a100426bd9b2451d492
Hackim18 <khakim668@gmail.com>
2025-08-04 17:01:42 +0700

    fix: fixing auth model

--- internal/model/auth_model.go
+++ internal/model/auth_model.go
@@ -1,10 +1,13 @@
 package model
 
-import "github.com/google/uuid"
+import (
+	"github.com/google/uuid"
+	"gorm.io/datatypes"
+)
 
 type Auth struct {
-	ID       uuid.UUID `json:"id"`
-	Username string    `json:"username"`
-	Email    string    `json:"email"`
-	Roles    []string  `json:"roles"`
+	ID       uuid.UUID      `json:"id"`
+	Username string         `json:"username" validate:"required,min=3,max=50"`
+	Email    string         `json:"email" validate:"required,email"`
+	Roles    datatypes.JSON `json:"roles" validate:"required,dive,required"`
 }
