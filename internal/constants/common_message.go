fb1e63679aa1307576e0048f47fd87249e200c27
Hackim18 <khakim668@gmail.com>
2025-08-04 13:07:54 +0700

    feat: add constant configuration

---
+++ internal/constants/common_message.go
@@ -0,0 +1,38 @@
+package constants
+
+import "golectro-user/internal/model"
+
+var (
+	WelcomeMessage = model.Message{
+		"en": "Welcome to Golectro User API",
+		"id": "Selamat datang di Golectro User API",
+	}
+	NotFound = model.Message{
+		"en": "API not found",
+		"id": "API tidak ditemukan",
+	}
+	FailedDataFromBody = model.Message{
+		"en": "Failed to get data from body",
+		"id": "Gagal mendapatkan data dari body",
+	}
+	FailedInputFormat = model.Message{
+		"en": "Invalid input format",
+		"id": "Format input tidak valid",
+	}
+	FailedValidationOccurred = model.Message{
+		"en": "Validation error occurred",
+		"id": "Terjadi kesalahan validasi",
+	}
+	InvalidToken = model.Message{
+		"en": "Invalid token",
+		"id": "Token tidak valid",
+	}
+	InternalServerError = model.Message{
+		"en": "Internal server error",
+		"id": "Kesalahan internal server",
+	}
+	TooManyRequests = model.Message{
+		"en": "Too many requests, please try again later",
+		"id": "Terlalu banyak permintaan, silakan coba lagi nanti",
+	}
+)
---
+++ internal/constants/doc_url.go
@@ -0,0 +1,8 @@
+package constants
+
+const (
+	DocBaseURL           = "https://api.example.com/docs"
+	DocRegisterURL       = DocBaseURL + "/register"
+	DocLoginURL          = DocBaseURL + "/login"
+	DocToManyRequestsURL = DocBaseURL + "/to-many-requests"
+)
