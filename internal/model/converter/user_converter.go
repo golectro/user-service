35067d1fd1df30738550f006d6abf6e465dfb724
Hackim18 <khakim668@gmail.com>
2025-08-04 17:37:43 +0700

    fix: fixing user model and converter

--- internal/model/converter/user_converter.go
+++ internal/model/converter/user_converter.go
@@ -7,6 +7,7 @@ import (
 
 func UserToResponse(user *entity.User) *model.UserSyncResponse {
 	return &model.UserSyncResponse{
+		ID:       user.ID,
 		Email:    user.Email,
 		Username: user.Username,
 	}
--- internal/model/user_model.go
+++ internal/model/user_model.go
@@ -1,12 +1,13 @@
 package model
 
+import "github.com/google/uuid"
+
 type VerifyUserRequest struct {
 	Token string `validate:"required,max=500"`
 }
 
 type UserSyncResponse struct {
-	ID        string `json:"id"`
-	Email     string `json:"email"`
-	Username  string `json:"username"`
-	CreatedAt int64  `json:"created_at"`
+	ID       uuid.UUID `json:"id"`
+	Email    string    `json:"email"`
+	Username string    `json:"username"`
 }
